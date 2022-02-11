74720e1a50715bd8df783748e2858e5513344e81
Rocio Rey <60386636+rociorey@users.noreply.github.com>
2022-02-11 10:42:03 +0000

    Add files via upload

--- EmotionsWebsite/public/data.js
+++ EmotionsWebsite/public/data.js
@@ -1,1 +1,71 @@
+let socket = io();
+let newText = document.getElementById('newText');
 
+
+// new Twitch.Player("twitch-embed", {
+//   channel: "rocioreyaloe",
+//   height:400,
+//   width:620
+// });
+
+
+//  Setting up the button on click
+const buttonHappy = document.getElementById('sendWordHappy');
+buttonHappy.addEventListener('click',event => {
+
+  // Getting the word input
+  // const mood = document.getElementById('moodHappy').value;
+  const mood = "happy";
+  // Getting the current date
+  const date = new Date().toLocaleString();
+  // const date = new Intl.DateTimeFormat('en-GB', { dateStyle: 'full', timeStyle: 'long' }).format(dateOld);
+  console.log(date);
+
+  const data = {date, mood};
+  // console.log(data);
+  socket.emit('userInputData', data);
+
+  // document.getElementById('moodHappy').value='';
+  
+
+})
+
+const buttonSad = document.getElementById('sendWordSad');
+buttonSad.addEventListener('click',event => {
+
+  // Getting the word input
+  // const mood = document.getElementById('moodHappy').value;
+  const mood = "sad";
+  // Getting the current date
+  const date = new Date().toLocaleString();
+  // const date = new Intl.DateTimeFormat('en-GB', { dateStyle: 'full', timeStyle: 'long' }).format(dateOld);
+  console.log(date);
+
+  const data = {date, mood};
+  // console.log(data);
+  socket.emit('userInputData', data);
+
+  // document.getElementById('moodSad').value='';
+  
+
+})
+
+const buttonLonely = document.getElementById('sendWordLonely');
+buttonLonely.addEventListener('click',event => {
+
+  // Getting the word input
+  // const mood = document.getElementById('moodHappy').value;
+  const mood = "lonely";
+  // Getting the current date
+  const date = new Date().toLocaleString();
+  // const date = new Intl.DateTimeFormat('en-GB', { dateStyle: 'full', timeStyle: 'long' }).format(dateOld);
+  console.log(date);
+
+  const data = {date, moodLonely};
+  // console.log(data);
+  socket.emit('userInputData', data);
+
+  // document.getElementById('moodLonely').value='';
+  
+
+})
