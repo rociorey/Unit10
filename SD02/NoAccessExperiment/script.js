024c682835cdc2c53a864239de8f6c428876121a
Rocio Rey <60386636+rociorey@users.noreply.github.com>
2022-03-05 22:58:28 +0000

    Add files via upload

---
+++ SD02/NoAccessExperiment/script.js
@@ -0,0 +1,70 @@
+// // Working here: when typoing letters alert pops up
+
+// const triggerWords = ['dad', 'works'];
+// var typingKeys = [];
+
+// document.getElementsByTagName('body')[0].onkeyup = function(e) { 
+//    var ev = e || window.event;
+
+   
+//     if (ev.keyCode == 68) {
+//         typingKeys.push('d'); 
+//     } 
+//     if (ev.keyCode == 65){
+//     typingKeys.push('a');
+// }   
+//         console.log(typingKeys);
+//     // }
+
+// //    if(ev.keyCode == 68) {// && ev.ctrlKey === true) { //control+f
+//     if (typingKeys.length == 3){
+//                 alert('not found');
+//     }     
+// }   
+
+
+// / Up to here
+
+
+// $('#input').keyup(()=>{
+//     if($('#input').val() == "dad"){
+//       alert("no value");
+//     } else{
+//       $('#input').css({backgroundColor: 'your-color'});
+//     }
+//   });
+
+$( document ).ready(function() {
+    var triggerWords = ['dad', 'father','workshop','workshops', 'tools', 'tool', 'drill', 'coding', 'electronics', 'circuit', 'wood', 'guidance', 'hammer'];
+    $(document).on('keyup', 'input', function() {
+        for (var i = 0; i < triggerWords.length; i++) {
+            if ($(this).val().toLowerCase().indexOf(triggerWords[i]) != -1) {
+                document.body.style.backgroundColor = "red";
+                var alertMsg ="Warning. You don't have access"
+                setTimeout(function(){alert(alertMsg)},100);
+                // alert("Warning. You don't have access");
+                setTimeout(function(){document.body.style.backgroundColor = "white"},100);
+                
+               
+                
+            }
+        }
+    });
+    
+    // $('button').click(function() {
+    //     var new_input = $('<input>');
+    //     new_input.insertAfter($(this));
+    // });
+    
+    
+});
+
+// When data is inputted into the element, trigger a callback function
+// document.getElementById("input").addEventListener("input", function(){
+//     // Check the value of the element
+//     if(input.value == "dad"){
+//       alert("no value");
+//     }else{
+//       input.style.background = "blue";
+//     }
+//   });
